e0cb7d8e669a307907172396e30bd54c9979d733
Mike Kistler <mikekistler@microsoft.com>
2024-03-27 16:13:11 -0500

    Fix script

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ rm -rf out; mkdir -p out
 awk -F',' '{print $3}' docs.csv | grep -v "None" | while read f; do
   # echo "Linting $f"
   out=$(echo $f | tr "/" "-" | sed 's/^specification-//' | sed 's/.json$/.out/')
-  spectral lint -r one_rule.yaml azure-rest-api-specs/$f > out/${out} && rm out/${out}
+  spectral lint -r onerule.yaml azure-rest-api-specs/$f > out/${out} && rm out/${out}
 done
